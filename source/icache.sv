73c6c70c25794d89a86bce92f4fa1be61b55b3dd
Patrick Thomison <pthomiso@purdue.edu>
2016-10-17 14:16:50 -0400

    PUSHING IT EMILY

---
+++ source/icache.sv
@@ -0,0 +1,32 @@
+/*
+  Patrick Thomison
+*/
+
+// interface include
+`include "cache_control_if.vh"
+
+// memory types
+`include "cpu_types_pkg.vh"
+
+module icache (
+  input CLK, nRST,
+  datapath_cache_if.cache dcif,
+  caches_if.caches cif
+);
+
+import cpu_types_pkg::*;
+
+icachef_t datasource [15:0];
+icachef_t iaddr;
+logic enable;
+
+assign reqAddr = dcif.iaddr;
+assign enable  = 
+
+always_ff @(posedge clk, negedge nRST) begin
+	if(nRST == 0) begin
+		datasource <= '0;
+	end else begin
+		 <= ;
+	end
+end
